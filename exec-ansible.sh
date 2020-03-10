76ebf5da1f4c62474e37d0722306691a37ea354d
t1una <totugekiluna@gmail.com>
2020-03-10 19:52:16 +0900

    [move] playbook-update(only update task)

--- exec-ansible.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ansible-playbook -i ansible/ansible-host.yml ansible/playbook.yml -K
--- exec-docker.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker-compose run --rm ansible ansible-playbook -i ansible-host.yml playbook.yml
