10bcb849b0ccaf6d9928dab06b6ce6e1df8d307e
g00chy <totugekiluna@gmail.com>
2020-10-08 16:02:16 +0900

    #3 add: pi4s add: arm64 docker server

--- exec-install-docker.sh
+++ exec-install-docker.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 docker-compose run --rm ansible ansible-playbook -i playbook/ansible-host.yml playbook/playbook-docker.yml
+docker-compose run --rm ansible ansible-playbook -i playbook/ansible-host.yml playbook/playbook-docker-arm.yml
