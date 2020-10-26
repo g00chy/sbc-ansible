c426ddada3aed6316819e8a2be23b491c5a10327
g00chy <totugekiluna@gmail.com>
2020-10-26 19:33:20 +0900

    change: add build platform

--- docker/python/build.sh
+++ docker/python/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker buildx build -t registry.localdomain/hugo:latest linux/amd64,linux/arm64,linux/arm/v7 .
+docker buildx build -t registry.localdomain/ansible:latest --platform linux/amd64,linux/arm64,linux/arm/v7 .
